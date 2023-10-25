5b702f6868549c5b1653522794334815593e172f
zhhcs <zhanghao_phy@163.com>
2023-10-25 11:41:06 +0800

    rollback

--- src/runwasm.rs
+++ src/runwasm.rs
@@ -205,7 +205,8 @@ impl FuncConfig {
 fn cvt_params(param_type: String, params: Vec<String>) -> Result<Vec<wasmtime::Val>, Error> {
     let mut res = Vec::new();
     let mut ok = true;
-    if param_type.to_ascii_lowercase() == "i32" {
+    if param_type.to_ascii_lowercase() == "void" {
+    } else if param_type.to_ascii_lowercase() == "i32" {
         params.iter().for_each(|param| {
             match param.parse::<i32>() {
                 Ok(val) => res.push(wasmtime::Val::from(val)),
@@ -261,32 +262,17 @@ pub fn call_func_sync(env: Environment) -> Result<Duration, Error> {
     let mut store = Store::new(&env.engine, wasi);
     let instance = env.linker.instantiate(&mut store, &env.module)?;
 
-    if conf.export_func == "_start" {
-        if let Ok(caller) = instance.get_typed_func::<(), ()>(&mut store, "_start") {
-            match caller.call(&mut store, ()) {
-                Ok(_) => {
-                    let end = std::time::Instant::now();
-                    let time = end - start;
-                    return Ok(time);
-                }
-                Err(err) => return Err(err),
-            };
-        } else {
-            return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
-        }
+    if let Some(caller) = instance.get_func(&mut store, &conf.export_func) {
+        match caller.call(&mut store, &conf.params, &mut conf.results) {
+            Ok(_) => {
+                let end = std::time::Instant::now();
+                let time = end - start;
+                return Ok(time);
+            }
+            Err(err) => return Err(err),
+        };
     } else {
-        if let Some(caller) = instance.get_func(&mut store, &conf.export_func) {
-            match caller.call(&mut store, &conf.params, &mut conf.results) {
-                Ok(_) => {
-                    let end = std::time::Instant::now();
-                    let time = end - start;
-                    return Ok(time);
-                }
-                Err(err) => return Err(err),
-            };
-        } else {
-            return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
-        }
+        return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
     }
 }
 
@@ -319,59 +305,31 @@ pub fn call_func(
     let relative_deadline = Some(std::time::Duration::from_millis(conf.relative_deadline));
     // let task_unique_name = conf.task_unique_name.clone();
     let func_result = func_result.clone();
-    if conf.export_func == "_start" {
-        if let Ok(caller) = instance.get_typed_func::<(), ()>(&mut store, "_start") {
-            let func = move || match caller.call(&mut store, ()) {
-                Ok(_) => {
-                    func_result.set_result(&format!("{:?}", conf.results));
-                    func_result.set_completed();
-                    Ok(conf.results)
-                }
-                Err(err) => {
-                    tracing::warn!("run_wasm_error: {}", err);
-                    func_result.set_result(&format!("{:?}", err));
-                    func_result.set_completed();
-                    Err(err)
-                }
-            };
-            let id = rt.spawn(func, expected_execution_time, relative_deadline);
-            match id {
-                Ok(id) => {
-                    NAME_ID.with(|map| map.borrow_mut().insert(conf.task_unique_name.clone(), id));
-                    return Ok((id, conf.task_unique_name));
-                }
-                Err(err) => return Err(err.into()),
+    if let Some(caller) = instance.get_func(&mut store, &conf.export_func) {
+        let func = move || match caller.call(&mut store, &conf.params, &mut conf.results) {
+            Ok(_) => {
+                // tracing::info!("{}: results = {:?}", task_unique_name, conf.results);
+                func_result.set_result(&format!("{:?}", conf.results));
+                func_result.set_completed();
+                Ok(conf.results)
             }
-        } else {
-            return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
+            Err(err) => {
+                tracing::warn!("run_wasm_error: {}", err);
+                func_result.set_result(&format!("{:?}", err));
+                func_result.set_completed();
+                Err(err)
+            }
+        };
+        let id = rt.spawn(func, expected_execution_time, relative_deadline);
+        match id {
+            Ok(id) => {
+                NAME_ID.with(|map| map.borrow_mut().insert(conf.task_unique_name.clone(), id));
+                return Ok((id, conf.task_unique_name));
+            }
+            Err(err) => return Err(err.into()),
         }
     } else {
-        if let Some(caller) = instance.get_func(&mut store, &conf.export_func) {
-            let func = move || match caller.call(&mut store, &conf.params, &mut conf.results) {
-                Ok(_) => {
-                    // tracing::info!("{}: results = {:?}", task_unique_name, conf.results);
-                    func_result.set_result(&format!("{:?}", conf.results));
-                    func_result.set_completed();
-                    Ok(conf.results)
-                }
-                Err(err) => {
-                    tracing::warn!("run_wasm_error: {}", err);
-                    func_result.set_result(&format!("{:?}", err));
-                    func_result.set_completed();
-                    Err(err)
-                }
-            };
-            let id = rt.spawn(func, expected_execution_time, relative_deadline);
-            match id {
-                Ok(id) => {
-                    NAME_ID.with(|map| map.borrow_mut().insert(conf.task_unique_name.clone(), id));
-                    return Ok((id, conf.task_unique_name));
-                }
-                Err(err) => return Err(err.into()),
-            }
-        } else {
-            return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
-        }
+        return Err(wasmtime::Error::msg("Invalid_export_func").context("Invalid_export_func"));
     }
 }
 
